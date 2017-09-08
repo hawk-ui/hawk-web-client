da69c10dd4fc64740e3ca4d741070751cc258038
‘bisht-richa‘ <richa.bisht@suse.com>
2017-09-08 15:18:25 +0200

    UI: Remove bootstrap-vue and add jquesy js.

--- src/main.js
+++ src/main.js
@@ -3,17 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'
 import Resource from 'vue-resource'
 import i18n from './i18n'
 import VueMaterial from 'vue-material'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import 'roboto-fontface-woff/css/roboto/roboto-fontface.css'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(VueMaterial)
-Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 Vue.use(Resource)
