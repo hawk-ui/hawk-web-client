0a1e239eeff620b53d47847bce8fa6aa6009c78d
‘bisht-richa‘ <richa.bisht@suse.com>
2017-09-08 17:25:02 +0200

    UI: Remove CDN link from index and implement jquery plugin

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Resource from 'vue-resource'
 import i18n from './i18n'
 import VueMaterial from 'vue-material'
+import jQuery from 'jquery'
+global.jQuery = jQuery
+let Bootstrap = require('bootstrap')
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import 'roboto-fontface-woff/css/roboto/roboto-fontface.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
 Vue.use(VueMaterial)
+Vue.use(Bootstrap)
 Vue.config.productionTip = false
 
 Vue.use(Resource)
