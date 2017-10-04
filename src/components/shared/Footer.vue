d6c50c2f7d712faa473062fac79a4ec7f2052c8e
‘bisht-richa‘ <richa.bisht@suse.com>
2017-10-04 16:55:31 +0200

    UI: Fix sidebar UI and add footer menu

--- src/components/shared/Footer.vue
+++ src/components/shared/Footer.vue
@@ -1,14 +1,21 @@
 <template>
-  <nav class="navbar fixed-bottom navbar-light bg-faded">
-    <div class="footer-simulator">
-      {{ $t("leftnav_bar.footer.simulator") }} <span class="pull-right"><md-icon class="brand-colors">visibility</md-icon></span>
-    </div>
-    <div class="footer-links">
-      <ul>
-        <li><md-icon class="md-16">assignment_ind</md-icon><span>{{ $t("leftnav_bar.footer.links.role") }}</span></li>
-        <li><md-icon class="md-16">grade</md-icon><span>{{ $t("leftnav_bar.footer.links.target") }}</span></li>
-        <li><md-icon class="md-16">help</md-icon><span>{{ $t("leftnav_bar.footer.links.help") }}</span></li>
-      </ul>
+  <nav class="navbar fixed-bottom">
+    <div class="footer-nav">
+      <div class="footer-copyright">{{ $t("leftnav_bar.footer.copy_right") }}</div>
+      <div class="dropup pull-right">
+        <div class="dropdown-toggle" data-toggle="dropdown">
+          <md-icon class="pull-right more-btn">more_vert</md-icon>
+        </div>
+        <ul class="dropdown-menu footer-dropdown">
+          <li class="footer-simulator">
+            {{ $t("leftnav_bar.footer.simulator") }} 
+            <span class="pull-right"><md-icon class="md-16 brand-colors">visibility</md-icon></span>
+          </li>
+          <li><md-icon class="md-14">assignment_ind</md-icon><span>{{ $t("leftnav_bar.footer.links.role") }}</span></li>
+          <li><md-icon class="md-14">grade</md-icon><span>{{ $t("leftnav_bar.footer.links.target") }}</span></li>
+          <li><md-icon class="md-14">help</md-icon><span>{{ $t("leftnav_bar.footer.links.help") }}</span></li>
+        </ul>
+      </div>
     </div>
   </nav>
 </template>
