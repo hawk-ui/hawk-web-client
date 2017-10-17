f759e1e3d969b3393cf8fb2ad660fca3d3d63855
ayoub-belarbi <belarbi.ayoub@gmail.com>
2017-10-17 11:31:37 +0200

    Dev: Importe VueRouter instead of Router
    According to the documentation, the name of the module is VueRouter and not Router.

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
 import Login from '@/components/Login'
 import MainLayout from '@/components/MainLayout'
 import Monitoring from '@/components/monitoring/Monitoring'
@@ -7,9 +7,9 @@ import Troubleshooting from '@/components/troubleshooting/Troubleshooting'
 import Configuration from '@/components/clusterConfiguration/Cluster-configuration.vue'
 import Hello from '@/components/Hello'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
+export default new VueRouter({
   mode: 'history',
   routes: [
     {
