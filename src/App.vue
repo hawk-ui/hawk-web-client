83a54c860b4c10207df3af757e9b33cdb163647a
ayoub-belarbi <belarbi.ayoub@gmail.com>
2017-10-17 14:08:48 +0200

    Dev: Update Vue.app to reflect the changes in routes.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
   <div id="wrapper">
-    <router-view></router-view>
+    <div>
+      <router-view name="left-sidebar"></router-view>
+      <div id="page-content-wrapper">
+        <div class="page-content container-fluid">
+          <router-view name="page-header"></router-view>
+          <div class="page-inner">
+            <router-view></router-view>
+          </div>
+        </div>
+      </div>
+    </div>    
   </div>
 </template>
 
 <script>
-
-export default {
-}
+  export default {
+    mounted: function () {
+      $('#menu-toggle').click(function (e) {
+        e.preventDefault()
+        $('#wrapper').toggleClass('toggled')
+      })
+    }
+  }
 </script>
 
 <style lang="scss">
-
+  @import './assets/css/layout.scss';
+  @import './assets/css/common.scss';
 </style>
