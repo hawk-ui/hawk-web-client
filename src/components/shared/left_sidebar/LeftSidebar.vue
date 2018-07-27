dbe804c3506330b0e11d94c775f68734440cbaa1
‘bisht-richa‘ <richa.bisht@suse.com>
2018-07-27 12:46:00 +0200

    UI: Small fixes

--- src/components/shared/left_sidebar/LeftSidebar.vue
+++ src/components/shared/left_sidebar/LeftSidebar.vue
@@ -4,20 +4,17 @@
       <ul class="no-list-style">
           <li>
             <router-link to="/monitoring">
-              <i class="material-icons md-18 leftnav_icon">desktop_mac</i>
-              {{ $t("leftnav_bar.links.monitoring") }}
+              <i class="material-icons md-18 leftnav_icon">desktop_mac</i>{{ $t("leftnav_bar.links.monitoring") }}
             </router-link>
           </li>
           <li>
             <router-link to="/troubleshooting/reports">
-              <i class="material-icons md-18 leftnav_icon">tune</i>
-              {{ $t("leftnav_bar.links.trubleshooting") }}
+              <i class="material-icons md-18 leftnav_icon">tune</i>{{ $t("leftnav_bar.links.trubleshooting") }}
             </router-link>
           </li>
           <li>
             <router-link to="/configuration">
-              <i class="material-icons md-18 leftnav_icon">settings</i>
-              {{ $t("leftnav_bar.links.configuration") }}
+              <i class="material-icons md-18 leftnav_icon">settings</i>{{ $t("leftnav_bar.links.configuration") }}
             </router-link>
           </li>
         </ul>
