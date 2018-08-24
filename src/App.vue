eee1ea253c2ef9b4311e14a04e0d8c23b3437557
‘bisht-richa‘ <richa.bisht@suse.com>
2018-08-24 16:05:10 +0200

    UI: create layout.vue and deleted duplication use of components in route.js

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,13 @@
 <template>
   <div id="wrapper">
     <router-view v-if="this.$route.path === '/login'"></router-view>
-    <div class="sidebar-view" v-else>
-      <router-view name="left-sidebar"></router-view>
-      <div id="page-content-wrapper">
-        <router-view name="simulator-panel"></router-view>
-        <div class="page-content page-content-right">
-          <router-view name="page-header"></router-view>
-            <router-view></router-view>
-        </div>
-      </div>
-    </div>
+    <app-layout v-else></app-layout>
   </div>
 </template>
 
-
 <script>
   import { mapGetters, mapActions } from 'vuex'
+  import Layout from './components/layout.vue'
   export default {
     data: function () {
       return {
@@ -33,6 +24,9 @@
     },
     mounted: function () {
       this.updateCib()
+    },
+    components: {
+      'app-layout': Layout
     }
   }
 </script>
--- src/router/route.js
+++ src/router/route.js
@@ -4,15 +4,9 @@ import VueRouter from 'vue-router'
 // Login page
 import Login from '@/components/Login'
 
-// Main structure (once logged in)
-import LeftSidebar from '@/components/shared/left_sidebar/leftSidebar'
-import PageHeader from '@/components/shared/page-header'
-import SimulatorPanel from '@/components/shared/simulator'
-
 // Main sections
 import Monitoring from '@/components/monitoring/Monitoring'
 import Troubleshooting from '@/components/troubleshooting/Troubleshooting'
-import CommandLog from '@/components/troubleshooting/CommandLog'
 import Configuration from '@/components/configuration/Configuration'
 import NodeStatus from '@/components/node/NodeStatus'
 import ResourceStatus from '@/components/resources/resourceStatus'
@@ -28,66 +22,27 @@ export default new VueRouter({
     },
     {
       path: '/',
-      components: {
-        default: Monitoring,
-        'page-header': PageHeader,
-        'left-sidebar': LeftSidebar,
-        'simulator-panel': SimulatorPanel
-      }
+      component: Monitoring
     },
     {
       path: '/monitoring',
-      components: {
-        default: Monitoring,
-        'page-header': PageHeader,
-        'left-sidebar': LeftSidebar,
-        'simulator-panel': SimulatorPanel
-      }
+      component: Monitoring
     },
     {
-      path: '/troubleshooting/reports',
-      components: {
-        default: Troubleshooting,
-        'page-header': PageHeader,
-        'left-sidebar': LeftSidebar,
-        'simulator-panel': SimulatorPanel
-      }
-    },
-    {
-      path: '/troubleshooting/commandLog',
-      components: {
-        default: CommandLog,
-        'page-header': PageHeader,
-        'left-sidebar': LeftSidebar,
-        'simulator-panel': SimulatorPanel
-      }
+      path: '/troubleshooting',
+      component: Troubleshooting
     },
     {
       path: '/configuration',
-      components: {
-        default: Configuration,
-        'page-header': PageHeader,
-        'left-sidebar': LeftSidebar,
-        'simulator-panel': SimulatorPanel
-      }
+      component: Configuration
     },
     {
       path: '/nodeStatus',
-      components: {
-        default: NodeStatus,
-        'page-header': PageHeader,
-        'left-sidebar': LeftSidebar,
-        'simulator-panel': SimulatorPanel
-      }
+      component: NodeStatus
     },
     {
       path: '/resourceStatus',
-      components: {
-        default: ResourceStatus,
-        'page-header': PageHeader,
-        'left-sidebar': LeftSidebar,
-        'simulator-panel': SimulatorPanel
-      }
+      component: ResourceStatus
     }
   ]
 })
