b992df6c375b0deb3eaaa40886ddcc167303bcbd
‘bisht-richa‘ <richa.bisht@suse.com>
2018-08-24 16:05:10 +0200

    UI: create layout.vue and deleted duplication use of components in route.js

---
+++ src/components/layout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="sidebar-view">
+    <app-left-sidebar></app-left-sidebar>
+    <div id="page-content-wrapper">
+      <app-simulator></app-simulator>
+      <div class="page-content page-content-right">
+        <app-page-header></app-page-header>
+          <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import LeftSidebar from './shared/left_sidebar/LeftSidebar.vue'
+  import PageHeader from './shared/page-header.vue'
+  import Simulator from './shared/simulator.vue'
+
+export default {
+    components: {
+      'app-left-sidebar': LeftSidebar,
+      'app-page-header': PageHeader,
+      'app-simulator': Simulator
+    }
+}
+
+</script>
+
+<style lang="scss">
+</style>
